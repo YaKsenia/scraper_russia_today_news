c69574401de298cf3041f8dd9d49fd89dc7fa898
Ksenia <xenia.iakovleva@gmail.com>
2019-05-08 20:06:01 +0200

    test

--- get_articles.py
+++ get_articles.py
@@ -7,20 +7,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-df = pd.read_csv('/home/ksenia/Desktop/thesis_scripts/RT_links_final.csv', sep=',', low_memory=False, na_values = ['no info', '.'])
+driver = webdriver.Firefox()
+df = pd.read_csv('/home/ksenia/Desktop/thesis_scripts/RT_links_merged.csv', sep=',', low_memory=False, na_values = ['no info', '.'])
 links = list(df['links'])
 headers = []
 texts = []
 for url in links:
+
 		article = Article(url)
 		article.download()
 		article.parse()
 		headers.append(article.title)
 		texts.append(article.text)
+		driver.implicitly_wait(5)
 
 final = pd.DataFrame({ 'title' : headers, 'text' : texts})
 
-#final.to_csv('RT_articles.csv')
+#final.to_csv('RT_articles_final.csv')
 
 
 #(article.html)
