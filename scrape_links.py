02ae98d8ecd916fdafd14d97972ed6322e5bc3da
YaKsenia <xenia.iakovleva@gmail.com>
2025-02-09 21:00:03 +0100

    updated libraries and functions that were not up to date anymore. Also, removed code for subscroption pop-up because it does not exist anymore on the website

--- scrape_links.py
+++ scrape_links.py
@@ -3,13 +3,9 @@ import urllib.request as urllib2
 import re
 import requests
 import pandas as pd
-from headers import HEADERS
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -23,23 +19,28 @@ def get_links(url):
 		driver = webdriver.Firefox()
 		driver.get(url)
 		driver.maximize_window()
-		driver.find_element_by_link_text('Подтвердить').click()
+		#driver.find_element_by_link_text('Подтвердить').click()
+		driver.find_element("link text", 'Подтвердить').click()
 		print('Closed cookies window')
-		driver.find_element_by_class_name('subscribe-layout')
-		driver.find_element_by_class_name('subscribe__close').click()
-		print('Closed subscription window')
+		#driver.find_element_by_class_name('subscribe-layout')
+		#driver.find_element(By.CLASS_NAME, "subscribe-layout")
+		#driver.find_element_by_class_name('subscribe__close').click()
+		#driver.find_element(By.CLASS_NAME, 'subscribe__close').click()
+		
+		#print('Closed subscription window')
 
 
 		for i in range(0,5):
 			print('Waiting for 5 seconds')
 			WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Загрузить ещё")))
-			driver.find_element_by_link_text('Загрузить ещё').click()
+			#driver.find_element_by_link_text('Загрузить ещё').click()
+			driver.find_element("link text", 'Загрузить ещё').click()
+
 			print('Clicked on "Load more"')
 
 
 		html = driver.page_source
 		html = BeautifulSoup(html, "lxml")
-
 		articles = html.find('div', {'class': 'listing__content listing__content_js'})
 		ankor_list = articles.findChildren('a')
 		
